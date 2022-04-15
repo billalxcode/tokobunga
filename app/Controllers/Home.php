c237a27fd765acb0a385bfe703410c1f09a00613
billalxcode <billal.xcode@gmail.com>
2022-04-15 10:00:16 +0700

    New update

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,11 +2,46 @@
 
 namespace App\Controllers;
 
+use App\Models\CategoryModel;
+use App\Models\DiscountModel;
 use App\Models\ProductModel;
+use App\Models\UserModel;
+use Config\Services;
 
 class Home extends BaseController
 {
+    protected $session;
     protected $context = array();
+    protected $helpers = [];
+    protected $userModel;
+    protected $productModel;
+    protected $categoryModel;
+    protected $discountModel;
+
+    function __construct()
+    {
+        $this->session = Services::session();
+
+        $this->userModel = new UserModel();
+        $this->productModel = new ProductModel();
+        $this->categoryModel = new CategoryModel();
+        $this->discountModel = new DiscountModel();
+    }
+
+    function checkSession() {
+        $isLogin = $this->session->get("login");
+        if ($isLogin) {
+            $email = $this->session->get("email");
+            $userData = $this->userModel->where("email", $email)->first();
+            if ($userData) {
+                return $userData;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 
     public function index()
     {
@@ -19,11 +54,20 @@ class Home extends BaseController
     }
 
     public function admin() {
-        $this->context["title"] = "Dashboard";
+        $userData = $this->checkSession();
+        if ($userData) {
+            $this->context["title"] = "Dashboard";
+            $this->context['products'] = $this->productModel->findAll();
+            $this->context['categories'] = $this->categoryModel->findAll();
+            $this->context['discounts'] = $this->discountModel->findAll();
 
-        echo view("admin/layout/header", $this->context);
-        echo view("admin/layout/sidebar");
-        echo view("admin/dashboard");
-        echo view("admin/layout/footer");
+            echo view("admin/layout/header", $this->context);
+            echo view("admin/layout/sidebar");
+            echo view("admin/dashboard");
+            echo view("admin/layout/footer");
+        } else {
+            $this->session->setFlashdata("error", "Maaf anda belum login");
+            return redirect()->to("/login");
+        }
     }
 }
---
+++ app/Views/admin/users/viewUser.php
@@ -0,0 +1,101 @@
+<div class="content-wrapper">
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0"><?= $title ?></h1>
+                </div>
+
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="<?= base_url('admin') ?>">Home</a></li>
+                        <li class="breadcrumb-item"><a href="<?= base_url('admin/users') ?>">Users</a></li>
+                        <li class="breadcrumb-item active">View</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="card shadow">
+                <div class="card-body">
+                    <table id="users" class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Username</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $index = 1;
+                            foreach ($users as $user) : ?>
+                                <tr>
+                                    <td><?= $index++ ?></td>
+                                    <td><?= $user['username'] ?></td>
+                                    <td><?= $user['name'] ?></td>
+                                    <td><?= $user['email'] ?></td>
+                                    <td>
+                                        <div class="d-flex d-inline">
+                                            <button class="btn btn-sm btn-danger mx-1" onclick="handleDelete('<?= $user['id'] ?>')">DELETE</button>
+                                            <!-- <button class="btn btn-sm btn-primary mx-1">UPDATE</button> -->
+                                        </div>
+                                    </td>
+                                </tr>
+                            <?php endforeach ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+
+<div class="modal fade" id="modal-delete">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Are you sure?</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="<?= base_url('admin/users/delete') ?>" method="post">
+                <input type="hidden" name="userid" value="" id="userid">
+                <div class="modal-body">
+                    <p>Dengan ini saya setuju menghapus user.</p>
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Yes</button>
+                </div>
+            </form>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
+
+<script>
+    $(function() {
+        $('#users').DataTable({
+            "paging": true,
+            "ordering": true,
+            "info": true,
+            "responsive": true,
+        });
+    });
+
+    function handleDelete(userid) {
+        $(document).ready(function() {
+            $("#userid").val(userid)
+            $("#modal-delete").modal("show")
+        })
+    }
+</script>
